55b4416c5ee0dfee6d802d4672175ed73dfb5c32
may-h <hms9210@naver.com>
2021-04-16 17:29:55 +0900

    first page component create

---
+++ src/components/page.ts
@@ -0,0 +1,15 @@
+export class PageComponent {
+    private element: HTMLUListElement; // 카드 목록을 담는다. 
+
+    constructor() {
+        this.element = document.createElement('ul');
+        this.element.setAttribute('class', 'page');
+        this.element.textContent = 'This is PageComponent';
+
+    }
+
+    //page component API 
+    attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+        parent.insertAdjacentElement(position, this.element);
+    }
+}
